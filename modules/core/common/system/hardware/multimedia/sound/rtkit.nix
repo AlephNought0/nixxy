26425a94899102cf0ca3e926252b6e37912f4017
AlephNought <radkohruska@gmail.com>
2024-05-17 13:47:47 +0200

    Fix the combustion node issue

--- modules/core/common/system/hardware/multimedia/sound/rtkit.nix
+++
@@ -1,6 +0,0 @@
-{config, ...}: {
-  # able to change scheduling policies, e.g. to SCHED_RR
-  # sounds server use RealtimeKit (rtkit) to acquire
-  # realtime priority
-  security.rtkit.enable = config.services.pipewire.enable;
-}
